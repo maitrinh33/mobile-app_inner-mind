9744af789617bee27b3f02e05ac91bcb350dbed8
maitrinh33 <trinh.do220909@vnuk.edu.vn>
2025-05-29 02:18:00 +0700

    git commit -m "feat(auth): implement login screen layout and logic"

---
+++ app/src/main/java/com/miu/meditationapp/LoginActivity.kt
@@ -0,0 +1,153 @@
+package com.miu.meditationapp
+
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.miu.meditationapp.databinding.ActivityLoginBinding
+
+
+class LoginActivity : AppCompatActivity() {
+    companion object {
+        val TAG = "login"
+    }
+    private lateinit var binding: ActivityLoginBinding
+    lateinit var mAuth:FirebaseAuth
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+    private lateinit var preferences: SharedPreferences
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+    private var showOneTapUI = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance()
+        oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                .setSupported(true)
+                .build())
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.firebaseid))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build()
+
+        binding.signInButton.setOnClickListener {
+            oneTapClient.beginSignIn(signInRequest)
+                .addOnSuccessListener(this) { result ->
+                    try {
+                        startIntentSenderForResult(
+                            result.pendingIntent.intentSender, REQ_ONE_TAP,
+                            null, 0, 0, 0, null)
+                    } catch (e: IntentSender.SendIntentException) {
+                        Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                    }
+                }
+                .addOnFailureListener(this) { e ->
+                    Log.d(TAG, e.localizedMessage)
+                }
+        }
+
+        preferences = getSharedPreferences("ONBOARD", Context.MODE_PRIVATE)
+
+        binding.registerTV.setOnClickListener {
+            startActivity(Intent(this, LoginAddUser::class.java))
+        }
+        binding.forgotPassword.setOnClickListener {
+            startActivity(Intent(this, LoginForgotPassword::class.java))
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val email: String = binding.inputEmail.text.toString()
+            val pwd: String = binding.inputPassword.text.toString()
+            if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pwd)) {
+                Toast.makeText(this, "Please enter your credentials", Toast.LENGTH_SHORT).show()
+            } else {
+                mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        binding.progressbar.visibility = View.GONE
+                        Toast.makeText(this, "Login Successful..", Toast.LENGTH_SHORT).show()
+                        startMainActivity()
+                    } else {
+                        binding.progressbar.visibility = View.GONE
+                        Toast.makeText(this, "Email or Password is wrong. Please try again.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with Firebase.
+                            Log.d(TAG, "Got ID token.")
+                        }
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d(TAG, "No ID token!")
+                        }
+                    }
+                } catch (e: ApiException) {
+                    // ...
+                }
+            }
+        }
+    }
+    private fun startMainActivity() {
+        if (isSeenOnbaord()) {
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            startActivity(Intent(this, OnboardingActivity::class.java))
+        }
+        finish()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var user: FirebaseUser? = mAuth.currentUser
+        if (user != null) {
+            startMainActivity()
+        }
+    }
+
+    fun isSeenOnbaord(): Boolean {
+        return preferences.getBoolean("ISCOMPLETE", false)
+    }
+}
